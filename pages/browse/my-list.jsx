5f29e90f78eb9b848bb63a1c9cb65ef95b00f134
Emmanuel Foeh <EmmanuelFo@stlghana.com>
2023-01-18 22:43:54 +0000

    added navbar

---
+++ pages/browse/my-list.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const myList = () => {
+  return (
+    <div>my-list</div>
+  )
+}
+
+export default myList
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 import Banner from "@/components/Banner/Banner";
+import Navbar from "@/components/Navbar/Navbar";
 
 
 export default function Home() {
@@ -13,8 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Netflix</h1>
-
+<Navbar username={"emmanuelfo"}/>
       <Banner
         title="Clifford the red dog"
         subTitle="a very cute dog"
